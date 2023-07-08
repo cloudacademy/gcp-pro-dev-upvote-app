0b72e3539275f8a040ae4265dc00164ce545c0ca
lrakai <lmrakai@gmail.com>
2023-07-07 23:05:03 -0600

    use API backend

--- src/VoteButton.js
+++ src/VoteButton.js
@@ -4,11 +4,27 @@ function VoteButton() {
   const [showMessage, setShowMessage] = useState(false);
 
   function handleClick() {
-    console.log('Vote cast!');
-    setShowMessage(true);
-    setTimeout(() => {
-      setShowMessage(false);
-    }, 1000);
+    fetch(process.env.REACT_APP_VOTE_API_URL, { 
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ vote: 1 })
+    }).then (response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('Something went wrong ...');
+      }
+    }).then(data => {
+      console.log(data);
+      setShowMessage(true);
+      setTimeout(() => {
+        setShowMessage(false);
+      }, 1000);
+    }).catch(error => {
+      console.error(error);
+    });
   }
 
   return (
